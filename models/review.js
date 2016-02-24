78d9ccd43e5a19ea950c5513e017baf4fd4c8caf
Michael Sawyer <michaelsawyer92@gmail.com>
2016-02-23 21:18:40 -0800

    Adds database and some styling.

---
+++ models/review.js
@@ -0,0 +1,21 @@
+/**
+ * Created by masstamike on 2/21/16.
+ */
+// models/review.js
+// grab the mongoose module
+var mongoose = require('mongoose');
+
+// define our nerd model
+// module.exports allows us to pass this to other files when it is called
+module.exports = mongoose.model('Review', {
+    beer : {type : String, default: ''},
+    brewer : {type : String, default: ''},
+    price : {type: Number, default: 0},
+    sampled : {type: Date, default: Date()},
+    rating : {type: Number, default: 0, min: 0, max: 5},
+    notes: {type: String, default: ''},
+    abv: {type: Number, default: 0},
+    ibu: {type: Number, default: 0},
+    servingType: {type: String, default: ''},
+
+});
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/beer-journal');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
