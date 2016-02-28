87c8e24155957179a5da4781732c8a0ad5d820b0
Michael Sawyer <michaelsawyer92@gmail.com>
2016-02-27 16:49:24 -0800

    Adds data binding. Stars stay lit.

--- public/javascripts/controllers/reviews.js
+++ public/javascripts/controllers/reviews.js
@@ -5,5 +5,20 @@
 
 var app = angular.module('beerJournalApp');
 app.controller('ReviewsCtrl', function($scope, $http) {
+    $scope.beerName = '';
+    $scope.brewer = '';
+    $scope.price = 0;
+    $scope.sampleDate = new Date();
+    $scope.rating = 0;
+    $scope.notes;
+    $scope.abv;
+    $scope.ibu;
+    $scope.servingType;
+    $scope.flavorWheel = {};
 
+    $scope.ratingGold = function (place) {
+        if (place <= $scope.rating) {
+            return "gold";
+        }
+    };
 });
