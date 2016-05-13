552d89bcc75198a1b0a2d0a0292327434e8e268c
Michael Sawyer <michaelsawyer92@gmail.com>
2016-05-12 22:59:32 -0700

    Adds test of type for getNumber

--- test/sample.spec.js
+++ test/sample.spec.js
@@ -4,18 +4,28 @@ describe('Controller: AllReviewsCtrl', function() {
     beforeEach(module('beerJournalApp'));
 
     var $controller;
+    var $scope;
 
     beforeEach(inject(function(_$controller_){
         $controller = _$controller_;
+        $scope = {};
     }));
 
     describe('getNumber', function() {
+
+        var result;
+
+        beforeEach(inject(function() {
+            var controller = $controller('AllReviewsCtrl', {$scope: $scope});
+            result = $scope.getNumber(5);
+        }));
+
         it('Array of size 5 should be returned on input=5', function () {
-            var $scope = {};
-            var controller = $controller('AllReviewsCtrl', { $scope: $scope });
-            var result = $scope.getNumber(5);
             expect(result.length).toBe(5);
-            expect(result).isArray;
         });
+
+        it('Return type should be an Array', function() {
+            expect(result).isArray;
+        })
     });
 });
